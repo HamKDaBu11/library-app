46c8e09bb07216d00c410bc86277904087df9fd1
HamKDaBu11 <khawajahamza35@outlook.com>
2024-09-20 01:39:51 +0500

    added search functionality and filtering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,83 +2,51 @@
 
 import Card from '@/components/Card/Card';
 import NavBar from '@/components/NavBar/NavBar';
+import { NextUIProvider } from "@nextui-org/react";
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 const BOOKS = [
     {
         id: 1,
-        title: 'Murder Machine',
-        author: 'Jerry Capeci',
-        publishedDate: '24 January 1990',
-        genre: 'Crime'
+        title: 'Circe',
+        author: 'Madeline Miller',
+        publishedDate: '2018',
+        genre: 'Fantasy'
     },
     {
         id: 2,
-        title: 'Murder Machine',
-        author: 'Jerry Capeci',
-        publishedDate: '24 January 1990',
-        genre: 'Crime'
+        title: 'The Night Circus',
+        author: 'Erin Morgenshtern',
+        publishedDate: '2011',
+        genre: 'Fantasy'
     },
     {
         id: 3,
-        title: 'Murder Machine',
-        author: 'Jerry Capeci',
-        publishedDate: '24 January 1990',
-        genre: 'Crime'
+        title: 'Assassin Apprentice',
+        author: 'Robin Hobb',
+        publishedDate: '1995',
+        genre: 'Fantasy'
     },
     {
         id: 4,
-        title: 'Murder Machine',
-        author: 'Jerry Capeci',
-        publishedDate: '24 January 1990',
-        genre: 'Crime'
+        title: 'A Game of Thrones',
+        author: 'George R.R Martin',
+        publishedDate: '1996',
+        genre: 'Fantasy'
     },
     {
         id: 5,
-        title: 'Murder Machine',
-        author: 'Jerry Capeci',
-        publishedDate: '24 January 1990',
-        genre: 'Crime'
-    },
-    {
-        id: 6,
-        title: 'Murder Machine',
-        author: 'Jerry Capeci',
-        publishedDate: '24 January 1990',
-        genre: 'Crime'
-    },
-    {
-        id: 7,
-        title: 'Murder Machine',
-        author: 'Jerry Capeci',
-        publishedDate: '24 January 1990',
-        genre: 'Crime'
-    },
-    {
-        id: 8,
-        title: 'Murder Machine',
-        author: 'Jerry Capeci',
-        publishedDate: '24 January 1990',
-        genre: 'Crime'
-    },
-    {
-        id: 9,
-        title: 'Murder Machine',
-        author: 'Jerry Capeci',
-        publishedDate: '24 January 1990',
-        genre: 'Crime'
-    },
-    {
-        id: 10,
-        title: 'Murder Machine',
-        author: 'Jerry Capeci',
-        publishedDate: '24 January 1990',
-        genre: 'Crime'
-    },
+        title: 'A Storm of Swords',
+        author: 'George R.R Martin',
+        publishedDate: '1996',
+        genre: 'Fantasy'
+    }
 ]
 
 export default function BookList() {
     const router = useRouter();
+    const [updatedBooks, setUpdatedBooks] = useState(BOOKS);
 
     const createQueryString = (name: any, value: any) => {
         // debugger
@@ -88,29 +56,44 @@ export default function BookList() {
         return params
     }
 
+    function handleBooksFilter(e: any) {
+        // console.log("EVENT: ", e)
+        setUpdatedBooks(BOOKS.filter((book: any) => {
+            return (
+                book.title.toLowerCase().trim().includes(e.toLowerCase())) || book.author.toLowerCase().trim().includes(e.toLowerCase())
+        }))
+    }
+
+    console.log("UPDATED BOOKS: ", updatedBooks)
+
     return (
-        <div>
-            <NavBar />
-            <div className='flex justify-center mt-4'>
-                <ul>
-                    {BOOKS.map((book: any) => {
-                        return (
-                            // <li key={book.id}>{book.title}</li>
-                            <Card className="mb-1">
-                                <p className="font-normal text-center text-gray-700 dark:text-gray-400">{book.title}</p>
-                                <p className="font-normal text-center text-gray-700 dark:text-gray-400">{book.author}</p>
-                                <p className="font-normal text-center text-gray-700 dark:text-gray-400">{book.genre}</p>
-                                <div className="flex flex-col items-center">
-                                    <div className="flex mt-4 md:mt-6">
-                                        <button onClick={() => router.push('/editBook' + "?" + createQueryString('book_item', JSON.stringify(book)))} className="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Update Book</button>
-                                        <button onClick={() => router.push('/deleteBook')} className="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Delete Book</button>
+        <NextUIProvider>
+            <div>
+                <NavBar addBooksFilter={handleBooksFilter} />
+                <div className='flex justify-center mt-4'>
+                    <ul>
+                        {updatedBooks.length > 1 ? updatedBooks.map((book: any) => {
+                            return (
+                                // <li key={book.id}>{book.title}</li>
+                                <Card className="mb-1">
+                                    <p className="font-normal text-center text-gray-700 dark:text-gray-400">{book.title}</p>
+                                    <p className="font-normal text-center text-gray-700 dark:text-gray-400">{book.author}</p>
+                                    <p className="font-normal text-center text-gray-700 dark:text-gray-400">{book.genre}</p>
+                                    <div className="flex flex-col items-center">
+                                        <div className="flex mt-4 md:mt-6">
+                                            <button onClick={() => router.push('/editBook' + "?" + createQueryString('book_item', JSON.stringify(book)))} className="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Update Book</button>
+                                            <button onClick={() => router.push('/deleteBook')} className="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Delete Book</button>
+                                        </div>
                                     </div>
-                                </div>
-                            </Card>
-                        )
-                    })}
-                </ul>
+                                </Card>
+                            )
+                        })
+                        :
+                        <h1>Sorry, this book does not exist.</h1>
+                    }
+                    </ul>
+                </div>
             </div>
-        </div>
+        </NextUIProvider>
     )
 }
