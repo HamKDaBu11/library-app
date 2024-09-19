0e8dc7b1c640b6399cb13f08e213b412160dda39
HamKDaBu11 <khawajahamza35@outlook.com>
2024-09-19 21:26:32 +0500

    added Edit Book with Form and validations

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -3,15 +3,15 @@
 import { bookSchema } from '@/schemas';
 import { useFormik } from 'formik';
 
-const Form = () => {
+const Form = (props: any) => {
     // Pass the useFormik() hook initial form values and a submit function that will
     // be called when the form is submitted
     const formik = useFormik({
         initialValues: {
-            title: '',
-            author: '',
-            publishedDate: '',
-            genre: ''
+            title: props.book_data.title,
+            author: props.book_data.author,
+            publishedDate: props.book_data.publishedDate,
+            genre: props.book_data.genre
         },
         validationSchema: bookSchema,
         onSubmit: () => {
@@ -31,9 +31,9 @@ const Form = () => {
                 onBlur={formik.handleBlur}
                 onChange={formik.handleChange}
                 value={formik.values.title}
-                className={`${formik.errors.title && formik.touched.title ? 'border-rose-600' : ""} mt-1 w-full rounded-md border border-gray-200 shadow-sm sm:text-sm py-2`}
+                className={`${formik.errors.title && formik.touched.title ? 'border-rose-600' : ""} mt-1 w-full rounded-md border border-gray-200 shadow-sm sm:text-sm p-2`}
             />
-            {formik.errors.title && formik.touched.title && <p className='text-rose-600 text-xs'>{formik.errors.title}</p>}
+            {formik.errors.title && formik.touched.title && <p className='text-rose-600 text-xs'>{formik.errors.title as String}</p>}
             <label htmlFor='author' className='block text-xs font-medium text-gray-700 mt-2'>Author</label>
             <input
                 id="author"
@@ -42,9 +42,9 @@ const Form = () => {
                 onBlur={formik.handleBlur}
                 onChange={formik.handleChange}
                 value={formik.values.author}
-                className={`${formik.errors.author && formik.touched.author ? "border-rose-600" : ""} mt-1 w-full rounded-md border border-gray-200 shadow-sm sm:text-sm py-2`}
+                className={`${formik.errors.author && formik.touched.author ? "border-rose-600" : ""} mt-1 w-full rounded-md border border-gray-200 shadow-sm sm:text-sm p-2`}
             />
-            {formik.errors.author && formik.touched.author && <p className='text-rose-600 text-xs'>{formik.errors.author}</p>}
+            {formik.errors.author && formik.touched.author && <p className='text-rose-600 text-xs'>{formik.errors.author as String}</p>}
             <label htmlFor='publishedDate' className='block text-xs font-medium text-gray-700 mt-2'>Published Date</label>
             <input
                 id="publishedDate"
@@ -53,9 +53,9 @@ const Form = () => {
                 onBlur={formik.handleBlur}
                 onChange={formik.handleChange}
                 value={formik.values.publishedDate}
-                className={`${formik.errors.publishedDate && formik.touched.publishedDate ? 'border-rose-600' : ""} mt-1 w-full rounded-md border border-gray-200 shadow-sm sm:text-sm py-2`}
+                className={`${formik.errors.publishedDate && formik.touched.publishedDate ? 'border-rose-600' : ""} mt-1 w-full rounded-md border border-gray-200 shadow-sm sm:text-sm p-2`}
             />
-            {formik.errors.publishedDate && formik.touched.publishedDate && <p className='text-rose-600 text-xs'>{formik.errors.publishedDate}</p>}
+            {formik.errors.publishedDate && formik.touched.publishedDate && <p className='text-rose-600 text-xs'>{formik.errors.publishedDate as String}</p>}
             <label htmlFor='genre' className='block text-xs font-medium text-gray-700 mt-2'>Genre</label>
             <input
                 id="genre"
@@ -64,9 +64,9 @@ const Form = () => {
                 onBlur={formik.handleBlur}
                 onChange={formik.handleChange}
                 value={formik.values.genre}
-                className={`${formik.errors.genre && formik.touched.genre ? 'border-rose-600' : ""} mt-1 w-full rounded-md border border-gray-200 shadow-sm sm:text-sm py-2`}
+                className={`${formik.errors.genre && formik.touched.genre ? 'border-rose-600' : ""} mt-1 w-full rounded-md border border-gray-200 shadow-sm sm:text-sm p-2`}
             />
-            {formik.errors.genre && formik.touched.genre && <p className='text-rose-600 text-xs'>{formik.errors.genre}</p>}
+            {formik.errors.genre && formik.touched.genre && <p className='text-rose-600 text-xs'>{formik.errors.genre as String}</p>}
             <button type="submit" className='border w-full rounded-full my-4 py-2 bg-teal-400'>
                 <span>Submit</span>
             </button>
